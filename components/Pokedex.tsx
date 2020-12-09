c8522a80645917a51cd2660154fd985b8969a18f
Injoong Yoon <27440940+AttilaTheHen@users.noreply.github.com>
2020-12-09 18:51:28 +0000

    feat: Add new Pokedex component to render all pokemon

---
+++ components/Pokedex.tsx
@@ -0,0 +1,13 @@
+import * as WebBrowser from 'expo-web-browser';
+import React from 'react';
+import { StyleSheet, TouchableOpacity } from 'react-native';
+
+import { Text, View } from './Themed';
+
+export default function Pokedex() {
+  return (
+    <View>
+      <Text>Pokedex here</Text>
+    </View>
+  )
+}
