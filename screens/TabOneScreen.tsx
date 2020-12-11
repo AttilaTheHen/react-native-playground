057d1a817e1332ab686375b338365b08780f3e90
Injoong Yoon <27440940+AttilaTheHen@users.noreply.github.com>
2020-12-11 20:12:12 +0000

    feat: Remove alignItems and justifyContent style rules to set layout correctly

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -9,7 +9,6 @@ export default function TabOneScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <View>
         <Pokedex />
       </View>
@@ -20,13 +19,10 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
-    marginTop: 100,
   },
   separator: {
     marginVertical: 15,
