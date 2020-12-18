fb4661e09521a7ebddac8b5e1b0889a46f11ba07
Injoong Yoon <27440940+AttilaTheHen@users.noreply.github.com>
2020-12-18 01:06:18 +0000

    feat: Tweak getPokemon to accept optional id param

--- services/pokeApi.ts
+++ services/pokeApi.ts
@@ -1,7 +1,8 @@
-const getPokemon = () => {
-  const url = 'https://pokeapi.co/api/v2/pokemon?limit=151';
-  return fetch(url)
-    .then(res => res.json());
+const getPokemon = (id?: number) => {
+  const url = id
+    ? `https://pokeapi.co/api/v2/pokemon/${id}`
+    : 'https://pokeapi.co/api/v2/pokemon?limit=151';
+  return fetch(url).then(res => res.json());
 };
 
 export default getPokemon;
