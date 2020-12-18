ef9b244faff8057aa4915778b74f1e2004075f80
Injoong Yoon <27440940+AttilaTheHen@users.noreply.github.com>
2020-12-18 01:11:25 +0000

    feat: Add logic to fetch base experience in individual Pokemon component

--- screens/Pokemon.tsx
+++ screens/Pokemon.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-import { Text } from '../components/Themed';
+import { Text, View } from '../components/Themed';
 import { Props } from '../types';
+import getPokemon from '../services/pokeApi';
+
+interface singlePokemon {
+  base_experience: number;
+}
 
 const Pokemon = ({ navigation, route }: Props) => {
-  return <Text style={{ margin: 10 }}>{route.params.name}</Text>;
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState({} as singlePokemon);
+
+  useEffect(() => {
+    getPokemon(route.params.id)
+      .then(data => setData(data))
+      .catch(err => console.error(err))
+      .finally(() => setLoading(false));
+  }, []);
+
+  return (
+    <View>
+      <Text style={{ margin: 10 }}>{route.params.name}</Text>
+      <Text style={{ margin: 10 }}>{data.base_experience}</Text>
+    </View>
+  );
 };
 
 export default Pokemon;
