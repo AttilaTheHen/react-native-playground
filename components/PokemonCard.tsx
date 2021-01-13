9931c2da2fdf391cc857e2ab5657fe15ff310ff7
Injoong Yoon <27440940+AttilaTheHen@users.noreply.github.com>
2021-01-13 22:06:43 +0000

    feat: Build out new PokemonCard component

--- components/PokemonCard.tsx
+++ components/PokemonCard.tsx
@@ -1,15 +1,49 @@
 import React from 'react';
-import { Image } from 'react-native';
+import { Image, StyleSheet } from 'react-native';
 
 import { View, Text } from './Themed';
+import { Props } from '../types';
+import PokemonTypeColors from '../constants/PokemonTypeColors';
 
-const PokemonCard = ({ title, image }: { title: string; image: string }) => {
+const PokemonCard = ({
+  title,
+  image,
+  pokemonType,
+}: {
+  title: string;
+  image: string;
+  pokemonType: string | null;
+}) => {
   return (
-    <View>
-      <Image source={{ uri: image }} />
-      <Text>{title}</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>{title}</Text>
+      <Image style={styles.image} source={{ uri: image }} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    height: 250,
+    margin: 5,
+    backgroundColor: '#FFDE00',
+    borderRadius: 5,
+  },
+  title: {
+    marginTop: 15,
+    marginBottom: 5,
+    marginLeft: 10,
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  image: {
+    flex: 1,
+    resizeMode: 'contain',
+    backgroundColor: 'black',
+    margin: 10,
+    borderRadius: 5,
+  },
+});
 
 export default PokemonCard;
