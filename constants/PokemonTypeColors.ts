a7af5b1a2c5724f5fbb8ba9362a36fd5ecb09174
Injoong Yoon <27440940+AttilaTheHen@users.noreply.github.com>
2021-01-13 20:48:59 +0000

    feat: Add all other pokemon type colors

--- constants/PokemonTypeColors.ts
+++ constants/PokemonTypeColors.ts
@@ -4,6 +4,20 @@ interface Colors {
 
 const colors: Colors = {
   grass: '#78C850',
+  fire: '#F08030',
+  water: '#6890F0',
+  bug: '#A8B820',
+  normal: '#A8A878',
+  poison: '#A040A0',
+  electric: '#F8D030',
+  ground: '#E0C068',
+  fighting: '#C03028',
+  psychic: '#F85888',
+  rock: '#B8A038',
+  ghost: '#705898',
+  ice: '#98D8D8',
+  dragon: '#7038F8',
+  flying: '#A890F0',
 };
 
 export default colors;
